2a59f26f1901ed1235cf5ea30ec00cf741a98c3e
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-09-10 12:50:36 +0700

    UPDATE: Installed & Setting up tailwindcss to project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='w-full min-h-screen flex flex-col justify-center items-center bg-slate-800'>
+
+      <div className="flex items-center gap-6">
+        <h1 className="font-primary text-5xl text-fuchsia-400">yōkoso</h1>
+        
+          <h1 className="font-primary text-5xl text-fuchsia-400 text-shadow-[2px_2px_0px_rgb(255_255_255_/_1)]">senseigata, minasama</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+        <img src="/banner.png" alt="banner" className="size-[350px]" />
+
+    <div className="mt-24 flex flex-col gap-6">
+      <button className="bg-fuchsia-400 text-white w-[300px] py-2 rounded-full">Masuk sebagai siswa</button>
+      <button className="border border-fuchsia-400 text-white w-[300px] py-2 rounded-full">Masuk sebagai administrator</button>
+    </div>
+
+    </div>
   )
 }
 
